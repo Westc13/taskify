6a493ff896b7207288ab5956d158c1c8c2502586
ChristineWest <christinewwwest@gmail.com>
2023-01-26 15:17:42 -0500

    added useRef to shift the focus away from input field so the shading effect will go away when submit the task

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import "./styles.css";
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 // const InputField = ({todo, setTodo}:Props)=>{}
 // is another syntax for the same thing
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="input">
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
-        type="input"
+        ref={inputRef}
+        type="text"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
         placeholder="Enter a task"
