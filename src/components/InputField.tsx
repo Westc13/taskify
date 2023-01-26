5c66b74ba5a9e4e4d319783d702ab6697e7db626
ChristineWest <christinewwwest@gmail.com>
2023-01-26 14:39:14 -0500

    completed inputfield logic

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+// const InputField = ({todo, setTodo}:Props)=>{}
+// is another syntax for the same thing
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input-box" />
+      <input
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input-box"
+      />
       <button className="input-submit" type="submit">
         GO
       </button>
